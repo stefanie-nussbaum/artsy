7637336627c59fc6cac323913866e94b94e0a5e1
Stefanie <stefanien001@gmail.com>
2021-08-27 16:48:01 -0500

    product details built out

--- client/src/screens/ProductDetails.jsx
+++ client/src/screens/ProductDetails.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { getOneProduct } from '../services/products'
 
 export default function ProductDetails(props) {
-  // const { currentUser, products } = props
+  const [product, setProduct] = useState()
+  const { id } = useParams()
+  const { currentUser } = props
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const productData = await getOneProduct(id)
+      setProduct(productData)
+    }
+    fetchProduct()
+    //eslint-disable-next-line
+  }, [])
 
   return (
     <div>
-      product details
+      <div>
+        <img src={product?.img_url} alt={product?.name} />
+      </div>
+      <h2>{product?.name}</h2>
+      {currentUser?.id === product?.user_id && (
+        <Link to={`/products/${product?.id}/edit`}>Edit</Link>
+      )}
+      <h3>${product?.price}</h3>
+      <Link to={`/users/${product?.user_id}/products`}>{product?.user.username}</Link>
+      <p>{product?.description}</p>
     </div>
   )
 }
