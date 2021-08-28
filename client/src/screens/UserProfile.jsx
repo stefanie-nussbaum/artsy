3e869d80b39c245d3f0b57ed900215e694485507
Stefanie <stefanien001@gmail.com>
2021-08-28 14:56:10 -0500

    user profile still need to display

--- client/src/screens/UserProfile.jsx
+++ client/src/screens/UserProfile.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ProductCard from '../components/ProductCard'
 
 export default function UserProfile(props) {
+  const [usersProducts, setUsersProducts] = useState([])
   const { currentUser, products } = props
   const { id } = useParams()
+
+  useEffect(() => {
+    setUsersProducts(products.filter((product) => {
+      if (product?.user_id === id) {
+        return product
+      }
+      return null
+    }))
+  }, [])
+
   return (
     <div>
-
-      {products.map((product, key) => {
-        console.log(product)
-        {
-          product?.user_id === id && (
-            <div key={key}>
-              <ProductCard currentUser={currentUser} product={product} />
-            </div>
-          )
-        }
-
+      {usersProducts?.map((product, key) => {
+        return (
+          <div key={key}>
+            <ProductCard currentUser={currentUser} product={product} />
+          </div>
+        )
       })}
-
     </div>
   )
 }
